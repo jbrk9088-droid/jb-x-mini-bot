072e0327e7963e1742f635f430e3300aac1191d0
Jb papa 71 <jbrk9088@gmail.com>
2026-03-13 19:31:47 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+botname: "JB x Mini Bot",
+ownername: "JB PAPA 71 ☠️",
+ownernumber: "584169861331",
+telegram: "https://t.me/jb_papa71",
+channel: "https://whatsapp.com/channel/0029Vb69yTi5PO0rX16dFQ1L"
+}
